cab1aa04b242dd923851f122355b1bbf45f1c20a
Visperi <niko.matasaho@gmail.com>
2025-11-03 16:45:27 +0200

    Add colors to logging

--- utils.py
+++ utils.py
@@ -28,11 +28,47 @@ class LostUser:
         return datetime.fromtimestamp(self._time_lost, TZ_HELSINKI)
 
 
-def configure_logging():
-    logging.basicConfig(
-        format="[{asctime}] [{levelname}] {name}: {message}",
-        level=logging.INFO,
-        style="{",
-        datefmt="%Y-%m-%d %H:%M:%S"
+class LogFormatter(logging.Formatter):
+    """
+    A custom log formatter with log level specific colours.
+    """
+
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    output_format = "[{asctime}] [{levelname}] {name}: {message}"
+
+    LEVEL_COLOURS = (
+        (logging.DEBUG, grey),
+        (logging.INFO, grey),
+        (logging.WARNING, yellow),
+        (logging.ERROR, red),
+        (logging.CRITICAL, bold_red)
     )
+
+    FORMATTERS = {}
+    for level, colour in LEVEL_COLOURS:
+        FORMATTERS[level] = logging.Formatter(fmt=colour+output_format+reset, datefmt="%Y-%m-%d %H:%M:%S", style="{")
+
+    def format(self, record) -> str:
+        formatter = self.FORMATTERS.get(record.levelno)
+        return formatter.format(record)
+
+
+def configure_logging():
+    """
+    Configure logging. Sets logging level to logging.WARNING for httpx
+    (library used for API requests by the Telegram library.)
+    """
+
+    handler = logging.StreamHandler()
+    root_logger = logging.getLogger()
+
+    handler.setFormatter(LogFormatter())
+    root_logger.setLevel(logging.INFO)
+    root_logger.addHandler(handler)
+
+    # Ignore Telegram library logging in smaller level than warnings
     logging.getLogger("httpx").setLevel(logging.WARNING)
