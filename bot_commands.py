2557f651e24a32388451973a420232e26bc85684
Visperi <niko.matasaho@gmail.com>
2025-11-05 08:32:06 +0200

    Move static methods to top level functions
    Leave only actual commands inside BotCommands class.

--- bot_commands.py
+++ bot_commands.py
@@ -12,7 +12,83 @@ from utils import TZ_HELSINKI
 logger = logging.getLogger(__name__)
 current_year = datetime.now(TZ_HELSINKI).year
 NNN_START = datetime(current_year, 11, 1, tzinfo=TZ_HELSINKI)
-NNN_END= datetime(current_year, 12, 1, tzinfo=TZ_HELSINKI)
+NNN_END = datetime(current_year, 12, 1, tzinfo=TZ_HELSINKI)
+
+
+def format_time(days: int, hours: int, minutes: int, seconds: int) -> str:
+    substrings = []
+    if days > 0:
+        substrings.append(f"{days} päivää")
+    if hours > 0:
+        substrings.append(f"{hours} tuntia")
+    if minutes > 0:
+        substrings.append(f"{minutes} minuuttia")
+    substrings.append(f"{seconds} sekuntia")
+
+    return ", ".join(substrings)
+
+
+def calculate_time_diff(minuend: datetime, subtrahend: datetime) -> Tuple[int, int, int, int]:
+    """
+    Calculate difference between two datetime objects.
+
+    :param minuend: The minuend, i.e. where the second datetime is subtracted from.
+    :param subtrahend: Subtrahend, i.e. datetime that is subtracted from the first datetime.
+    :return: The difference as tuple of (days, hours, minutes, seconds).
+    """
+    diff = minuend - subtrahend
+    hours, remainder = divmod(diff.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+
+    return diff.days, hours, minutes, seconds
+
+
+def get_time_left() -> Optional[Tuple[int, int, int, int]]:
+    """
+    Calculate how much time there is left of NNN.
+
+    :return: Time left as tuple (days, hours, minutes, seconds), or None if NNN has not started.
+    """
+    current_time = datetime.now(TZ_HELSINKI)
+
+    if NNN_END < current_time:
+        return None
+
+    return calculate_time_diff(NNN_END, current_time)
+
+
+def get_time_gone() -> Optional[Tuple[int, int, int, int]]:
+    """
+    Calculate how long the NNN has been on.
+
+    :return: Time left as tuple (days, hours, minutes, seconds), or None if the NNN has already ended.
+    """
+    current_time = datetime.now(TZ_HELSINKI)
+
+    if NNN_START > current_time:
+        return None
+
+    return calculate_time_diff(current_time, NNN_START)
+
+
+def calculate_average_time(lose_times: List[datetime]) -> Optional[Tuple[int, int, int, int]]:
+    """
+    Calculate an average between datetime objects.
+
+    :param lose_times: List of datetime objects.
+    :return: Average time between datetime objects as tuple (days, hours, minutes, seconds), or None if the list is
+             empty.
+    """
+    if len(lose_times) == 0:
+        return None
+
+    total_seconds = 0
+    for dt in lose_times:
+        total_seconds += dt.timestamp()
+
+    avg = total_seconds / len(lose_times)
+    avg_dt = datetime.fromtimestamp(avg, TZ_HELSINKI)
+    return calculate_time_diff(avg_dt, NNN_START)
 
 
 class BotCommands:
@@ -20,79 +96,13 @@ class BotCommands:
     def __init__(self):
         self.db = DatabaseHandler("app.db")
 
-    @staticmethod
-    def format_time(days: int, hours: int, minutes: int, seconds: int) -> str:
-        substrings = []
-        if days > 0:
-            substrings.append(f"{days} päivää")
-        if hours > 0:
-            substrings.append(f"{hours} tuntia")
-        if minutes > 0:
-            substrings.append(f"{minutes} minuuttia")
-        substrings.append(f"{seconds} sekuntia")
-
-        return ", ".join(substrings)
-
-    @staticmethod
-    def calculate_time_diff(minuend: datetime, subtrahend: datetime) -> Tuple[int, int, int, int]:
-        """
-        Calculate difference between two datetimes.
-
-        :param minuend: The minuend, i.e. where the second datetime is subtracted from.
-        :param subtrahend: Subtrahend, i.e. datetime that is subtracted from the first datetime.
-        :return: The difference as tuple of (days, hours, minutes, seconds).
-        """
-        diff = minuend - subtrahend
-        hours, remainder = divmod(diff.seconds, 3600)
-        minutes, seconds = divmod(remainder, 60)
-
-        return diff.days, hours, minutes, seconds
-
-    def get_time_left(self) -> Optional[Tuple[int, int, int, int]]:
-        """
-        Get how much time there is left of NNN.
-
-        :return: Time left as tuple (days, hours, minutes, seconds), or None if NNN has not started.
-        """
-        current_time = datetime.now(TZ_HELSINKI)
-
-        if NNN_END < current_time:
-            return None
-
-        return self.calculate_time_diff(NNN_END, current_time)
-
-    def get_time_gone(self) -> Optional[Tuple[int, int, int, int]]:
-        """
-        Get how long the NNN has been on.
-
-        :return: Time left as tuple (days, hours, minutes, seconds), or None if the NNN has already ended.
-        """
-        current_time = datetime.now(TZ_HELSINKI)
-
-        if NNN_START > current_time:
-            return None
-
-        return self.calculate_time_diff(current_time, NNN_START)
-
-    def calculate_average_time(self, lose_times: List[datetime]) -> Optional[Tuple[int, int, int, int]]:
-        if len(lose_times) == 0:
-            return None
-
-        total_seconds = 0
-        for dt in lose_times:
-            total_seconds+= dt.timestamp()
-
-        avg = total_seconds / len(lose_times)
-        avg_dt = datetime.fromtimestamp(avg, TZ_HELSINKI)
-        return self.calculate_time_diff(avg_dt, NNN_START)
-
     async def time_left_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        time_left = self.get_time_left()
+        time_left = get_time_left()
         if not time_left:
             await update.effective_chat.send_message("Ohi on!")
             return
 
-        time_left_str = self.format_time(*time_left)
+        time_left_str = format_time(*time_left)
         await update.effective_chat.send_message(f"NNN:ää jäljellä:\n\n{time_left_str}")
 
     async def lost_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -100,12 +110,12 @@ class BotCommands:
             await update.effective_chat.send_message("Komento toimii vain kanavilla.")
             return
 
-        time_gone = self.get_time_gone()
+        time_gone = get_time_gone()
         if not time_gone:
             await update.effective_chat.send_message("NNN ei ole käynnissä.")
             return
 
-        time_gone_str = self.format_time(*time_gone)
+        time_gone_str = format_time(*time_gone)
         user = update.effective_user
 
         try:
@@ -137,11 +147,11 @@ class BotCommands:
         lost_users = self.db.get_losers(update.effective_chat.id)
         num_lost_users = len(lost_users)
         lost_percentage = round(num_lost_users / num_users * 100, 1)
-        avg_lost = self.calculate_average_time([user.time_lost for user in lost_users])
+        avg_lost = calculate_average_time([user.time_lost for user in lost_users])
         if not avg_lost:
             formatted_avg_lost = "Ei laskettavissa"
         else:
-            formatted_avg_lost = self.format_time(*avg_lost)
+            formatted_avg_lost = format_time(*avg_lost)
 
         msg = (f"Kanavan {update.effective_chat.effective_name} NNN-tilastot\n\n"
                f"Hävinneitä: {num_lost_users} ({lost_percentage} %)\n"
@@ -162,8 +172,8 @@ class BotCommands:
         lost_users.sort(key=lambda x: x.time_lost)
         placements = ""
         for i, user in enumerate(lost_users):
-            diff = self.calculate_time_diff(user.time_lost, NNN_START)
-            placements += f"{i+1}. {user.username.lstrip('@')}\t({self.format_time(*diff)})\n"
+            diff = calculate_time_diff(user.time_lost, NNN_START)
+            placements += f"{i+1}. {user.username.lstrip('@')}\t({format_time(*diff)})\n"
 
         await update.effective_chat.send_message(f"Näin vähän aikaa kanavan coomerit kesti:\n\n{placements}")
 
@@ -177,13 +187,13 @@ class BotCommands:
         tg_chat = update.effective_chat
         lost_user = self.db.get_lost_user(tg_user.id, tg_chat.id)
         if not lost_user:
-            time_gone = self.get_time_gone()
+            time_gone = get_time_gone()
             diff_days = (time_gone[0] * 24 + time_gone[1]) / 24
             await tg_chat.send_message(f"{tg_user.name} on kestänyt "
                                        f"jo {round(diff_days, 1)} vuorokautta! Hullu tyyppi.")
         else:
-            diff = self.calculate_time_diff(lost_user.time_lost, NNN_START)
-            formatted = self.format_time(*diff)
+            diff = calculate_time_diff(lost_user.time_lost, NNN_START)
+            formatted = format_time(*diff)
             msg = f"{tg_user.name} kesti NNN:ää yhteensä: {formatted}."
             await tg_chat.send_message(msg)
 
